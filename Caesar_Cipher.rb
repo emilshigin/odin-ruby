c16e594f4b8c0e8b37d62a5428a284dd701027b6
emilshigin <emilshigin@protonmail.com>
2022-01-25 16:11:29 -0800

    Optimized Ceasar Cipher

--- Caesar_Cipher.rb
+++ Caesar_Cipher.rb
@@ -1,13 +1,12 @@
 def ceasor_cipher (phrase, shift)
-  new_phrase = ""
-  phrase.split("").map do |letter|
-    if letter.match(/a-zA-Z/) || letter == letter.downcase then base = 97 else base = 65 end
-      letter = ((((letter.ord - base) + shift) % 26) + base).chr
-    new_phrase = new_phrase+letter  
+  new_phrase = phrase.split("").map do |letter|
+    if letter == letter.downcase then base = 97 else base = 65 end
+    letter = ((((letter.ord - base) + shift) % 26) + base).chr if letter.match(/[a-zA-Z]/)
+    letter
   end
-    
-  return new_phrase
+
+  new_phrase.flatten.join
 end
 
-p text = ceasor_cipher('What a string y HelloWorld!',-25)
+p text = ceasor_cipher('Hello_worlD!', 13)
 
